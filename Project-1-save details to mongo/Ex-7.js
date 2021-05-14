1213603665f3253d17f708b6d2b745f4878e72ff
Hari Prakash <56472426+hari-praks@users.noreply.github.com>
2021-05-14 12:12:10 +0530

    Add files via upload

---
+++ Project-1-save details to mongo/Ex-7.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/");
+var nameSchema = new mongoose.Schema({
+UserName: String, Password: String});
+var User = mongoose.model("User", nameSchema);
+app.get("/", (req, res) => {
+res.sendFile(__dirname + "/Ex-7.html");});
+app.post("/addname", (req, res) => {
+var myData = new User(req.body);
+myData.save()
+.then(item => {
+res.send("Your details saved to database"); })
+.catch(err => {
+res.status(400).send("Unable to save to database"); });});
+app.listen(port, () => {
+console.log("Server listening on port " + port);});
